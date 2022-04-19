90d418252f3ac7ac752466570b027e74eb3ba62c
Wiby Putra Adyan Ramdhani <wibbyramdhani@gmail.com>
2022-04-19 22:59:04 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+$share="https://app.oneaset.co.id/s/BvUZB3";
+$link="https://app.oneaset.co.id/finance/Finance/LandingPage?channel=web_OneAset_activity_financeinvite&referrerCode=WQR9R&source=outside&ad=Ym09MiZjcD01Jmd1PW51bGwmdWM9MTAmdWU9MCZ1aWQ9MjA3Mjc0";
+$ref="WQR9R";
+$delay="120";
